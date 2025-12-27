832933a85ae99c021ee6d18553113ca683e67f92
Meet-salvi <salvimeet4@gmail.com>
2025-12-27 18:01:15 +0530

    initial NestJS and PostgreSQL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -12,7 +14,33 @@ import { DoctorAvailabilityModule } from './doctor-availability/doctor-availabil
 import { PatientDetailsModule } from './patient-details/patient-details.module';
 
 @Module({
-  imports: [UsersModule, DoctorsModule, PatientsModule, ClinicsModule, AppointmentsModule, PaymentsModule, MessagesModule, DoctorAvailabilityModule, PatientDetailsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // PostgreSQL + TypeORM configuration
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(<string>process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, //set false in production
+    }),
+
+    UsersModule,
+    DoctorsModule,
+    PatientsModule,
+    ClinicsModule,
+    AppointmentsModule,
+    PaymentsModule,
+    MessagesModule,
+    DoctorAvailabilityModule,
+    PatientDetailsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
